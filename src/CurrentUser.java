7d9e49c5c92caa965b381311fc400a967c77e927
jacobfett03 <93690458+jacobfett03@users.noreply.github.com>
2022-04-14 13:08:19 -0700

    Add files via upload

---
+++ src/CurrentUser.java
@@ -0,0 +1,13 @@
+package application;
+
+import java.io.Serializable;
+
+public class CurrentUser implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	boolean loggedIn;
+	String username;
+	
+	
+
+}
